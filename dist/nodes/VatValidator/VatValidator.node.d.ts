7311fe31e1334e316771676c3369f4876896bc75
Joshua Heller <heller.joshua.lb@gmail.com>
2026-04-15 09:30:36 +0200

    Remove VatValidator node implementation and associated TypeScript definitions and source maps from the build output.

--- dist/nodes/VatValidator/VatValidator.node.d.ts
+++
@@ -1,6 +0,0 @@
-import { IExecuteFunctions, INodeExecutionData, INodeType, INodeTypeDescription } from 'n8n-workflow';
-export declare class VatValidator implements INodeType {
-    description: INodeTypeDescription;
-    execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]>;
-}
-//# sourceMappingURL=VatValidator.node.d.ts.map
